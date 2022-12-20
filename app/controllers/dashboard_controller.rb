f1a570ea8175bf78cf77910b874e521d3c59c8c2
João Otávio <joaotavioos@outlook.com>
2022-12-20 18:42:18 -0300

    feat: update and add links controller ✨

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,26 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show]
   
   def index
+    @should_render_navbar = true
+  end
+
+  def appearance
+    @should_render_navbar = true
+  end
+
+  def show
+    redirect_to dashboard_path if @user.nil?
+    @links = @user.links.where.not(url: '', title: '')
+  end
+
+  private
+
+  def set_user
+    # localhost:3000/1
+    @user = User.friendly.find(params[:id])
+  rescue StandardError
+    @user = nil
   end
 end
