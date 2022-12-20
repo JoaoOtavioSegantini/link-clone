ef07e6fda3fffe5e861f8fb482b0c3ac32560023
João Otávio <joaotavioos@outlook.com>
2022-12-20 18:43:49 -0300

    feat: update routes ✨

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get 'dashboard', to: 'dashboard#index'
+  get 'appearance', to: 'dashboard#appearance'
+  patch 'links/:id' => 'links#update', as: :links
+
+  root 'dashboard#index'
+
+  # Allows us to use user_path(user) to get the URL for a user's profile page
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "dashboard#index"
+  # root "articles#index"
 end
